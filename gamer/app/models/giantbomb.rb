6e4934c0182aa30d9c1fbc88e83893b08b46de5e
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-08-24 22:08:20 -0500

    Set up giantbomb initialization to pull giantbomb api.

--- gamer/app/models/giantbomb.rb
+++ gamer/app/models/giantbomb.rb
@@ -1,2 +1,5 @@
 class Giantbomb < ActiveRecord::Base
+  def initialize(game_id)
+    @results = HTTParty.get("http://www.giantbomb.com/api/game/#{game_id}/?api_key=#{ENV[GIANT_BOMB_KEY]}&format=json")
+  end
 end
